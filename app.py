5f0a969d0845f6c42b868e5bc6963e00abafdb82
karthicklogan93-hub <karthicklogan93@gmail.com>
2026-02-23 20:30:11 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/19e8BHXBQCD1pU4Me4wGJRiJFHlrGL5vW
 """
 
-!pip install streamlit geopy
 import streamlit as st
 from geopy.distance import geodesic
 import datetime
